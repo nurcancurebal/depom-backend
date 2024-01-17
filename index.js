e93758a0ec7be847c2318846ed2e6e1bd75c8712
nurcan <nurcancurebal@hotmail.com>
2024-01-17 19:13:35 +0300

    .

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+// ? Node modules.
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// ? Constants.
+const PORT = process.env.PORT || 3000;
+
+// ? Database connection and conf.
+mongoose.connection
+    .on('error', error => {
+        console.error("MongoDB", error);
+    })
+    .on('open', () => {
+        console.log("MongoDB", "Database Connected");
+    });
+
+mongoose.connect('mongodb://127.0.0.1:27017/StokTakip');
+
+// ? Express application create.
+const app = express();
+
+// ? Middlewares.
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+
+// ? Server listen.
+app.listen(PORT, () => {
+    console.log("Listening api server", `http://localhost:${PORT}`);
+});
