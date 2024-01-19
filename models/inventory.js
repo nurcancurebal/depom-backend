ade323d075dc4f9bb267665767e6498aef4c3dc5
nurcan <nurcancurebal@hotmail.com>
2024-01-19 12:02:50 +0300

    routes

---
+++ models/inventory.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const Model = mongoose.model('Inventory',
+    {
+        barcode: {
+            type: String,
+            required: true
+        },
+        productname: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        category: {
+            type: String,
+            required: true
+        },
+        subcategory: {
+            type: String,
+            required: true
+        },
+        supplier: {
+            type: String,
+            required: true
+        },
+        brand: {
+            type: String,
+            required: true
+        },
+        unit: {
+            type: String,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        unitprice: {
+            type: Number,
+            required: true
+        }
+    }
+);
+
+module.exports = Model;
