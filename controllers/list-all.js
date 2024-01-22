510d686d76fb13b2218e4e027224e4c1e2c11e7c
Nurcan Cürebal <nurcancurebal@hotmail.com>
2024-01-22 17:07:28 +0300

    list-all

--- controllers/list-all.js
+++ controllers/list-all.js
@@ -4,11 +4,7 @@ module.exports = async function (req, res, next) {
 
     try {
 
-        const body = req.body;
-
-        let result = await ModelInventory.find({
-            barcode: body.barcode,
-        });
+        let result = await ModelInventory.find();
 
         result = Array.from(result).map(item => item._doc);
 
