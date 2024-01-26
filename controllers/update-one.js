309e08c93a3e29be2d853a0ca64a0ffc5e58d5b1
Nurcan Cürebal <nurcancurebal@hotmail.com>
2024-01-26 23:08:16 +0300

    .

--- controllers/update-one.js
+++ controllers/update-one.js
@@ -7,7 +7,20 @@ module.exports = async function (req, res, next) {
         const barcode = req.params.barcode;
         const body = req.body;
 
-        const findOneInventory = await ModelInventory.updateOne({ barcode }, body);
+        const data = {
+            barcode,
+            productname: body.productname,
+            date: body.date,
+            category: body.category,
+            subcategory: body.subcategory,
+            supplier: body.supplier,
+            brand: body.brand,
+            unit: body.unit,
+            quantity: body.quantity,
+            unitprice: body.unitprice
+        };
+
+        const findOneInventory = await ModelInventory.updateOne({ barcode }, data);
 
         if (!findOneInventory) {
             throw new Error("Not found inventory!!!");
