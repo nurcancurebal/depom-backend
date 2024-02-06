233e450f7771ce743b5e32f41f02ef16a07fc94d
Nurcan <nurcancurebal@hotmail.com>
2024-02-06 18:34:57 +0300

    delete

--- routes/inventory.js
+++ routes/inventory.js
@@ -5,6 +5,5 @@ router.get("/", require('../controllers/list-all'));
 router.get("/:barcode", require('../controllers/list-barcode'));
 router.post("/", require('../controllers/entry-one'));
 router.post("/:barcode", require('../controllers/checkout-one'));
-router.delete("/:barcode", require('../controllers/delete-one'));
 
 module.exports = router;
