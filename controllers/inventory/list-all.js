718dac7f245ec8c8b7079d023c723eac8278bc63
nurcan <nurcancurebal@hotmail.com>
2024-08-02 19:40:32 +0300

    .

--- controllers/inventory/list-all.js
+++ controllers/inventory/list-all.js
@@ -3,11 +3,15 @@ const ModelInventory = require("../../models/inventory");
 module.exports = async function (req, res, next) {
   try {
     const userId = res.locals.user._id;
-
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
     const sort = req.query.sort || "barcode";
     const skip = (page - 1) * limit;
+
+    if (!page) throw new Error("Sayfa bulunamadı!");
+    if (!limit) throw new Error("Limit bulunamadı!");
+    if (!sort) throw new Error("Sıralama bulunamadı!");
+    if (!skip && skip !== 0) throw new Error("Atlanacak sayfa bulunamadı!");
 
     const sortName = sort.startsWith("-") ? sort.slice(1) : sort;
 
